24099208bd99805277b7ae12cdc3c606305b4f44
h4nabii <leon_xenos.xu@foxmail.com>
2023-09-14 05:27:12 +0800

    创建基础布局以及全局导航栏

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="globalHeader">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/logo.png" alt="" />
+          <div class="title">花火OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title-bar .title {
+  color: #444;
+  margin-left: 16px;
+  font-size: 20px;
+}
+
+.title-bar .logo {
+  height: 48px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="basic-layout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+
+      <a-layout-footer class="footer">
+        <a href="">Link</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basic-layout {
+}
+
+#basic-layout .header {
+  background-color: skyblue;
+  margin-bottom: 16px;
+}
+
+#basic-layout .content {
+  background: linear-gradient(to right, #ddd, #fff);
+  margin-bottom: 16px;
+}
+
+#basic-layout .footer {
+  background-color: #efefef;
+  padding: 16px;
+
+  /* 底部居中 */
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
