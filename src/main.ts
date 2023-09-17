9579e1950295f2f1a6130e7e5cd25641168d25ce
h4nabii <leon_xenos.xu@foxmail.com>
2023-09-17 12:09:10 +0800

    引入 [typemd](https://github.com/bytedance/bytemd) 组件库

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import store from "./store";
 // Arco design for vue
 import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
+import "bytemd/dist/index.css";
 import "@/access";
 
 createApp(App).use(ArcoVue).use(store).use(router).mount("#app");
