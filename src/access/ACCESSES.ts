f9c23e45961bb7fa67f2bd1a67cb9f5177bb2a99
h4nabii <leon_xenos.xu@foxmail.com>
2023-09-20 11:12:31 +0800

    修改ACCESSES为enum

--- src/access/ACCESSES.ts
+++ src/access/ACCESSES.ts
@@ -1,7 +1,7 @@
-const ACCESSES = {
-  NOT_LOGIN: "notLogin",
-  USER: "user",
-  ADMIN: "admin",
-};
+enum ACCESSES {
+  NOT_LOGIN = "notLogin",
+  USER = "user",
+  ADMIN = "admin",
+}
 
 export default ACCESSES;
