ed41407361f4380dec4df692b07baf0137ba1805
h4nabii <leon_xenos.xu@foxmail.com>
2023-09-20 11:14:22 +0800

    重新规划路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "@/router/routes";
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env["BASE_URL"]),
   routes,
 });
 
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,13 +1,17 @@
+// libs
 import { RouteRecordRaw } from "vue-router";
-import NoAuthView from "@/views/NoAuthView.vue";
 import ACCESSES from "@/access/ACCESSES";
+
+// layouts
 import UserLayout from "@/layouts/UserLayout.vue";
+
+// views
+import NoAuthView from "@/views/NoAuthView.vue";
 import UserLoginView from "@/views/user/UserLoginView.vue";
 import UserRegisterView from "@/views/user/UserRegisterView.vue";
+import QuestionsView from "@/views/question/QuestionsView.vue";
 import AddQuestionView from "@/views/question/AddQuestionView.vue";
 import ManageQuestionView from "@/views/question/ManageQuestionView.vue";
-import AboutView from "@/views/AboutView.vue";
-import QuestionsView from "@/views/question/QuestionsView.vue";
 import ViewQuestionsView from "@/views/question/ViewQuestionsView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
@@ -15,6 +19,54 @@ export const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "主页",
     component: QuestionsView,
+  },
+  {
+    path: "/questions",
+    name: "题目列表",
+    component: QuestionsView,
+  },
+  {
+    path: "/add/question",
+    name: "创建题目",
+    component: AddQuestionView,
+    meta: {
+      access: ACCESSES.USER,
+    },
+  },
+  {
+    path: "/manage/question",
+    name: "管理题目",
+    component: ManageQuestionView,
+    meta: {
+      access: ACCESSES.USER,
+    },
+  },
+  {
+    path: "/view/questions/:id",
+    name: "在线做题",
+    component: ViewQuestionsView,
+    props: true,
+    meta: {
+      hideInMenu: true,
+      access: ACCESSES.USER,
+    },
+  },
+  {
+    path: "/update/question",
+    name: "更新题目",
+    component: AddQuestionView,
+    meta: {
+      hideInMenu: true,
+      access: ACCESSES.ADMIN,
+    },
+  },
+  {
+    path: "/noAuth",
+    name: "无权限",
+    component: NoAuthView,
+    meta: {
+      hideInMenu: true,
+    },
   },
   {
     path: "/user",
@@ -35,58 +87,5 @@ export const routes: Array<RouteRecordRaw> = [
         component: UserRegisterView,
       },
     ],
-  },
-  {
-    path: "/add/question",
-    name: "创建题目",
-    component: AddQuestionView,
-    meta: {
-      access: ACCESSES.USER,
-    },
-  },
-  {
-    path: "/manage/question",
-    name: "管理题目",
-    component: ManageQuestionView,
-    meta: {
-      access: ACCESSES.USER,
-    },
-  },
-  {
-    path: "/questions",
-    name: "浏览题目",
-    component: QuestionsView,
-  },
-  {
-    path: "/view/questions/:id",
-    name: "在线做题",
-    component: ViewQuestionsView,
-    props: true,
-    meta: {
-      hideInMen: true,
-      access: ACCESSES.USER,
-    },
-  },
-  {
-    path: "/update/question",
-    name: "更新题目",
-    component: AddQuestionView,
-    meta: {
-      access: ACCESSES.ADMIN,
-      hideInMenu: true,
-    },
-  },
-  {
-    path: "/noAuth",
-    name: "无权限",
-    component: NoAuthView,
-    meta: {
-      hideInMenu: true,
-    },
-  },
-  {
-    path: "/about",
-    name: "关于我的",
-    component: AboutView,
   },
 ];
