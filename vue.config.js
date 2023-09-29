3f103ee435c67117198d270bfeae4ec669e5daf3
h4nabii <leon_xenos.xu@foxmail.com>
2023-09-29 22:34:59 +0800

    build: change folder name

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   chainWebpack(config) {
     config.plugin("monaco").use(new MonacoWebpackPlugin());
   },
+  outputDir: "docs",
   publicPath:
     process.env.NODE_ENV === "production" ? "/h4nabii-oj-frontend/" : "/",
 });
